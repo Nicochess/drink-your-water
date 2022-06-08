8ea736b9d11e03a66f35d7f324395ad796ea9545
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-06-08 12:31:45 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Drink Your Water</h1>
+      <p>2 Litros de Agua</p>
+      <span>Progress Bar</span>
+      <button>Tomei</button>
+    </div>
+  );
+}
+
+export default App;
