cf0d9f9d0268dbb2f6b7206c5f90541e67fd6ab6
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-06-09 16:46:31 -0300

    chore: time put

--- public/background.js
+++ public/background.js
@@ -1,15 +1,15 @@
 /* global chrome */
 
-chrome.alarms.create("drink water", {delayInMinutes: 1})
+chrome.alarms.create("drink water", {delayInMinutes: 120})
 
 chrome.alarms.onAlarm.addListener(() => {
-    createNotification(clearNotifications)
+    createNotification("popup", clearNotifications)
 })
 
-const createNotification = (callback) => {
-    callback()
+const createNotification = (id, callback) => {
+    callback(id)
 
-    chrome.notifications.create("popup", {
+    chrome.notifications.create(id, {
         type: "basic",
         iconUrl: "./icons/water128.png",
         title: "Hora de beber água",
@@ -21,6 +21,6 @@ const createNotification = (callback) => {
     })
 }
 
-const clearNotifications = () => {
-    chrome.notifications.clear("popup")
+const clearNotifications = (id) => {
+    chrome.notifications.clear(id)
 }
